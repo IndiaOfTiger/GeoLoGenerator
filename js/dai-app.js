31a353ccf61c61192d4a1215f61306afa8c417a6
Cosey <apple@140-113-89-226.Dorm9.NCTU.edu.tw>
2016-12-16 03:47:56 +0800

    Finished page appearance

--- js/dai-app.js
+++ js/dai-app.js
@@ -2,27 +2,15 @@ $(function () {
     var r = 0;
     var g = 0;
     var b = 0;
-    var lum = 0;
 
-    function draw () {
-        var rr = Math.floor((r * lum) / 100);
-        var gg = Math.floor((g * lum) / 100);
-        var bb = Math.floor((b * lum) / 100);
-        $('.bulb-top, .bulb-middle-1, .bulb-middle-2, .bulb-middle-3, .bulb-bottom, .night').css(
-            {'background': 'rgb('+ rr +', '+ gg +', '+ bb +')'}
-        );
-    }
-
-    function Luminance (data) {
-        lum = data[0]
-        draw();
-    }
+    $('#submit').on('click', function(){
+        $('#submit').toggle(1000).toggle(1000);});
+    
 
     function Color_O (data) {
         r = data[0];
         g = data[1];
         b = data[2];
-        draw();
     }
 
     function iot_app () {
@@ -30,12 +18,12 @@ $(function () {
         g = 40;
         b = 40;
         lum = 100;
-        draw();
     }
 
     var profile = {
         'dm_name': 'GeoLoGenerator',
-        'df_list': [Luminance, Color_O],
+        'is_sim': false,
+        'df_list': ['Color_I'],
     }
 
     var ida = {
